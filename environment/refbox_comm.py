ce17df475e63cf0157a4546b8c85e33af648ccf4
Ian Khodachenko <ikhodachenko@student.tugraz.at>
2019-09-02 11:17:44 +0200

    added an additional process handling data

--- environment/refbox_comm.py
+++ environment/refbox_comm.py
@@ -5,6 +5,7 @@ import socket
 import time
 import struct
 import traceback
+import multiprocessing
 import datetime as dt
 from collections import OrderedDict
 from copy import deepcopy # more performant then dict()
@@ -105,6 +106,171 @@ def send_pb_message(pb_msg, s):
     
     return ret
 
+def connect_socket(s, exist=True):
+    if exist:
+        s.close()
+    
+    while True:
+        try:
+            # create socket and build connection
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((TCP_IP, TCP_PORT))
+            print("NEW Connection started: \n{}\n\n".format(s))
+            break
+        except ConnectionRefusedError:
+            print("Connection refused, trying again..")
+            s.close()
+            time.sleep(1)
+    
+    return s
+
+def communicator(debug_output=False, log_=False):
+    global s
+    
+    # create socket and build connection
+    s = connect_socket(None, False)
+    
+    if log_:
+        message_file = open("messages.log", "a")
+    
+    # work with global main variables (for now)
+    global machines
+    global orders
+    global rings
+    
+    last_machines = {}
+    
+    # manage unpacking messages
+    data = b""
+    while True:
+        try:
+            data += s.recv(BUFFER_SIZE)
+            # while there is something processable inside the buffer
+            have_data = True
+        except KeyboardInterrupt:
+            print("Worker got KeyboardInterrupt, exiting..")
+            data = b""
+            
+        if not data:
+            have_data = False
+            print("Connection lost, trying to reconnect")
+            s = connect_socket(s)
+        
+        try:
+            # greater then 12 as we need at least 12 bytes for headers
+            while have_data and len(data) >= 12:
+                ##### extract one message from data
+                message = dict(PACKET_LAYOUT)
+                pos = 0 # position inside current data buffer
+                for field_name, field_size in PACKET_LAYOUT.items():
+                    
+                    # as we are ordered the protobuf part is last
+                    if field_name == "protobuf_msg":
+                        #take the raw bytes
+                        protobuf_size = message["payload_size"] - 4
+                        # check if we have enough bytes for message
+                        if protobuf_size + 12 > len(data):
+                            have_data = False
+                            break
+                        message[field_name] = data[pos:pos + protobuf_size] # minus 4 for the header
+                        pos += protobuf_size
+                        continue
+                    
+                    # reconstructed byte part
+                    recon = data[pos:pos + field_size]
+                        
+                    # distinguish format
+                    form = create_byte_form(field_size)
+                    
+                    # convert to proper size
+                    up = struct.unpack(form, recon)
+                    assert len(up) == 1
+                    message[field_name] = up[0]
+        
+                    pos += field_size # current position
+                
+                if have_data == False:
+                    break
+                
+                ##### get the protobuf message object
+                try:
+                    component = COMPONENTS[(message["component_ID"], message["message_type"])]
+                except KeyError as e:
+                    print("We cound not find given component ID and message type combination!\n{}".format(message))
+                    raise e
+                
+                ##### proccess incoming protobuff messages
+                if component not in NOT_NEED:
+                    # create the protobuf object of the appropriate type
+                    pb_obj_constructor = getattr(getattr(pb, component + "_pb2"), component)
+                    pb_obj = pb_obj_constructor() # a new object
+                    read = pb_obj.ParseFromString(message["protobuf_msg"])
+                    assert read == message["payload_size"] - 4
+                
+                    if component == "MachineInfo":
+                        # reset to default values
+                        # TODO: has instructions field - do we need?
+                        machines = {"CS1": deepcopy(MACHINE),
+                                    "CS2": deepcopy(MACHINE),
+                                    "RS1": deepcopy(MACHINE),
+                                    "RS2": deepcopy(MACHINE),
+                                    "SS": deepcopy(MACHINE),
+                                    "BS": deepcopy(MACHINE),
+                                    "DS": deepcopy(MACHINE)}
+                        
+                        for m in pb_obj.machines:
+                            # filter just one activve team; here CYAN
+                            if m.name[0] == "C":
+                                mtype = m.name[2:] # string for type
+                                
+                                machines[mtype]["state"] = m.state
+                                machines[mtype]["loaded"] = m.loaded_with
+                                machines[mtype]["ring_colors"] = list(m.ring_colors)
+                                # process the lights
+                                for x in m.lights:
+                                    # 0 is OFF, 1 is ON, 2 is BLINK => MachineDescripton.proto
+                                    machines[mtype]["lights"][x.color] = x.state
+                        
+                        outp = [[k, v] for k, v in machines.items() if k != "SS"] # just for debug
+                        if debug_output and last_machines != machines: # also just for debug...
+                            print("We got a <{}> message:\n{}\n{}\n{}".format(component, outp[:2], outp[2:4], outp[4:]))
+                        last_machines = deepcopy(machines)
+                    
+                    elif component == "OrderInfo":
+                        orders = pb_obj.orders # we can work with given struct here as need all param
+                        if debug_output:
+                            print("We got a <{}> message:\n{}".format(component, pb_obj))
+                    elif component == "GameState":
+                        if debug_output:
+                            print("We got a <{}> message:\nphase={}".format(component, pb_obj.phase))
+                    elif component == "RingInfo":
+                        # TODO: technically only need to know once
+                        rings = {1 : 0,
+                                 2 : 0,
+                                 3 : 0,
+                                 4 : 0}
+                        for r in pb_obj.rings:
+                            rings[r.ring_color] = r.raw_material
+                        if debug_output:
+                            print("We got a <{}> message:\n{}".format(component, rings))
+                    else:
+                        print("We got a <{}> message:\n{}".format(component, pb_obj))
+                    
+                    if log_:
+                        message_file.write("----------------------------------------------------------\n{} - <{}>:\n{}".format(dt.datetime.now(), component, pb_obj))                
+        
+                # remove proccessed message from the buffer
+                data = data[pos:]
+        except Exception as e:
+            print("HAD EXCEPTION:\n{}".format(e))
+            print(traceback.format_exc())
+            raise e
+
+    if log_:
+        message_file.close()
+        
+    print("Workerprocess closed")
+
 
 def simulate_game():
     # TODO: currently we just assume that message arrived - check that?
@@ -186,139 +352,14 @@ def simulate_game():
 # TODO: seems like the RefBox has a MOCKUP mode but it is not properly working and the machines do get timeouts
 
 if __name__ == "__main__":
-    # create socket and build connection
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((TCP_IP, TCP_PORT))
-    print("Connection started: \n{}\n\n".format(s))
+    # start the thread handling current data
+    p = multiprocessing.Process(target=communicator, args=(False, False,))
+    p.start()
     
-    message_file = open("messages.log", "a")
+    try:
+        while True:
+            time.sleep(5)
+    except KeyboardInterrupt:
+        p.terminate()
+        print("Main got KeyboardInterrupt, exiting..")
     
-    # work with global main variables (for now)
-    global machines
-    global orders
-    global rings
-    
-    last_machines = {}
-    
-    # manage unpacking messages
-    data = b""
-    while True:
-        data += s.recv(BUFFER_SIZE)
-        
-        # while there is something processable inside the buffer
-        have_data = True
-        try:
-            # greater then 12 as we need at least 12 bytes for headers
-            while have_data and len(data) >= 12:
-                ##### extract one message from data
-                message = dict(PACKET_LAYOUT)
-                pos = 0 # position inside current data buffer
-                for field_name, field_size in PACKET_LAYOUT.items():
-                    
-                    # as we are ordered the protobuf part is last
-                    if field_name == "protobuf_msg":
-                        #take the raw bytes
-                        protobuf_size = message["payload_size"] - 4
-                        # check if we have enough bytes for message
-                        if protobuf_size + 12 > len(data):
-                            have_data = False
-                            break
-                        message[field_name] = data[pos:pos + protobuf_size] # minus 4 for the header
-                        pos += protobuf_size
-                        continue
-                    
-                    # reconstructed byte part
-                    recon = data[pos:pos + field_size]
-                        
-                    # distinguish format
-                    form = create_byte_form(field_size)
-                    
-                    # convert to proper size
-                    up = struct.unpack(form, recon)
-                    assert len(up) == 1
-                    message[field_name] = up[0]
-        
-                    pos += field_size # current position
-                
-                if have_data == False:
-                    break
-                
-                ##### get the protobuf message object
-                try:
-                    component = COMPONENTS[(message["component_ID"], message["message_type"])]
-                except KeyError as e:
-                    print("We cound not find given component ID and message type combination!\n{}".format(message))
-                    raise e
-                
-                ##### proccess incoming protobuff messages
-                if component not in NOT_NEED:
-                    # create the protobuf object of the appropriate type
-                    pb_obj_constructor = getattr(getattr(pb, component + "_pb2"), component)
-                    pb_obj = pb_obj_constructor() # a new object
-                    read = pb_obj.ParseFromString(message["protobuf_msg"])
-                    assert read == message["payload_size"] - 4
-                
-                    if component == "MachineInfo":
-                        # reset to default values
-                        # TODO: has instructions field - do we need?
-                        machines = {"CS1": deepcopy(MACHINE),
-                                    "CS2": deepcopy(MACHINE),
-                                    "RS1": deepcopy(MACHINE),
-                                    "RS2": deepcopy(MACHINE),
-                                    "SS": deepcopy(MACHINE),
-                                    "BS": deepcopy(MACHINE),
-                                    "DS": deepcopy(MACHINE)}
-                        
-                        for m in pb_obj.machines:
-                            # filter just one activve team; here CYAN
-                            if m.name[0] == "C":
-                                mtype = m.name[2:] # string for type
-                                
-                                machines[mtype]["state"] = m.state
-                                machines[mtype]["loaded"] = m.loaded_with
-                                machines[mtype]["ring_colors"] = list(m.ring_colors)
-                                # process the lights
-                                for x in m.lights:
-                                    # 0 is OFF, 1 is ON, 2 is BLINK => MachineDescripton.proto
-                                    machines[mtype]["lights"][x.color] = x.state
-                        
-                        outp = [[k, v] for k, v in machines.items() if k != "SS"] # just for debug
-                        if last_machines != machines: # also just for debug...
-                            print("We got a <{}> message:\n{}\n{}\n{}".format(component, outp[:2], outp[2:4], outp[4:]))
-                        last_machines = deepcopy(machines)
-                    
-                    elif component == "OrderInfo":
-                        orders = pb_obj.orders # we can work with given struct here as need all param
-                        print("We got a <{}> message:\n{}".format(component, pb_obj))
-                    elif component == "GameState":
-                        print("We got a <{}> message:\nphase={}".format(component, pb_obj.phase))
-                    elif component == "RingInfo":
-                        # TODO: technically only need to know once
-                        rings = {1 : 0,
-                                 2 : 0,
-                                 3 : 0,
-                                 4 : 0}
-                        for r in pb_obj.rings:
-                            rings[r.ring_color] = r.raw_material
-                        print("We got a <{}> message:\n{}".format(component, rings))
-                    else:
-                        print("We got a <{}> message:\n{}".format(component, pb_obj))
-#                time.sleep(1)
-#                    message_file.write("----------------------------------------------------------\n{} - <{}>:\n{}".format(dt.datetime.now(), component, pb_obj))
-#                else:
-#                    pb_obj_constructor = getattr(getattr(pb, component + "_pb2"), component)
-#                    pb_obj = pb_obj_constructor() # a new object
-#                    read = pb_obj.ParseFromString(message["protobuf_msg"])
-#                    assert read == message["payload_size"] - 4
-#                    message_file.write("----------------------------------------------------------\n{} - <{}>:\n{}".format(dt.datetime.now(), component, pb_obj))
-                
-        
-                # remove proccessed message from the buffer
-                data = data[pos:]
-        except Exception as e:
-            print("HAD EXCEPTION:\n{}".format(e))
-            print(traceback.format_exc())
-            raise e
-
-    message_file.close()
-    s.close()
