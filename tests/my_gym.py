9309c427589d65ac9121a9c6f75d1b4c10f9452c
Ian Khodachenko <ikhodachenko@student.tugraz.at>
2019-10-08 10:09:21 +0200

    removed dead code

--- tests/my_gym.py
+++ tests/my_gym.py
@@ -7,11 +7,9 @@ from keras.callbacks import TensorBoard
 from keras.optimizers import Adam
 #import keras.backend.tensorflow_backend as backend
 import tensorflow as tf
-import matplotlib.pyplot as plt
 
 from collections import deque
 from tqdm import tqdm
-from PIL import Image
 import time
 from datetime import datetime
 import numpy as np
@@ -52,162 +50,6 @@ class ModifiedTensorBoard(TensorBoard):
     # Creates writer, writes custom metrics and closes writer
     def update_stats(self, **stats):
         self._write_logs(stats, self.step)
-
-
-class Blob:
-    def __init__(self, size):
-        self.size = size
-        self.x = np.random.randint(0, size)
-        self.y = np.random.randint(0, size)
-
-    def __str__(self):
-        return f"Blob ({self.x}, {self.y})"
-
-    def __sub__(self, other):
-        return (self.x-other.x, self.y-other.y)
-
-    # so one can check if two blobs are overlapping
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def action(self, choice):
-        '''
-        Gives us 9 total movement options. (0,1,2,3,4,5,6,7,8)
-        '''
-        # diagonal
-        if choice == 0:
-            self.move(x=1, y=1)
-        elif choice == 1:
-            self.move(x=-1, y=-1)
-        elif choice == 2:
-            self.move(x=-1, y=1)
-        elif choice == 3:
-            self.move(x=1, y=-1)
-
-        # in x direction
-        elif choice == 4:
-            self.move(x=1, y=0)
-        elif choice == 5:
-            self.move(x=-1, y=0)
-
-        # in y direction
-        elif choice == 6:
-            self.move(x=0, y=1)
-        elif choice == 7:
-            self.move(x=0, y=-1)
-        
-        # dont move
-        elif choice == 8:
-            self.move(x=0, y=0)
-
-    def move(self, x=False, y=False):
-
-        # If no value for x, move randomly
-        if not x:
-            self.x += np.random.randint(-1, 2)
-        else:
-            self.x += x
-
-        # If no value for y, move randomly
-        if not y:
-            self.y += np.random.randint(-1, 2)
-        else:
-            self.y += y
-
-        # If we are out of bounds, fix!
-        if self.x < 0:
-            self.x = 0
-        elif self.x > self.size-1:
-            self.x = self.size-1
-        if self.y < 0:
-            self.y = 0
-        elif self.y > self.size-1:
-            self.y = self.size-1
-
-
-class BlobEnv:
-    SIZE = 10
-    RETURN_IMAGES = False
-    MOVE_PENALTY = 1
-    ENEMY_PENALTY = 100
-    FOOD_REWARD = 50
-    OBSERVATION_SPACE_VALUES = (SIZE, SIZE, 3)  # 4
-    ACTION_SPACE_SIZE = 9
-    PLAYER_N = 1  # player key in dict
-    FOOD_N = 2  # food key in dict
-    ENEMY_N = 3  # enemy key in dict
-    # the dict! (colors)
-    d = {1: (255, 175, 0),
-         2: (0, 255, 0),
-         3: (0, 0, 255)}
-
-    def reset(self):
-        self.player = Blob(self.SIZE)
-        self.food = Blob(self.SIZE)
-        while self.food == self.player:
-            self.food = Blob(self.SIZE)
-        self.enemy = Blob(self.SIZE)
-        while self.enemy == self.player or self.enemy == self.food:
-            self.enemy = Blob(self.SIZE)
-
-        self.episode_step = 0
-
-        if self.RETURN_IMAGES:
-            observation = np.array(self.get_image())
-        else:
-            observation = (self.player-self.food) + (self.player-self.enemy)
-        return observation
-
-    def step(self, action):
-        self.episode_step += 1
-        self.player.action(action)
-
-        #### MAYBE ###
-        #enemy.move()
-        #food.move()
-        ##############
-
-        if self.RETURN_IMAGES:
-            new_observation = np.array(self.get_image())
-        else:
-            new_observation = (self.player-self.food) + (self.player-self.enemy)
-
-        if self.player == self.enemy:
-            reward = -self.ENEMY_PENALTY
-        elif self.player == self.food:
-            reward = self.FOOD_REWARD
-        else:
-            reward = -self.MOVE_PENALTY
-
-        done = False
-        if reward == self.FOOD_REWARD or reward == - self.ENEMY_PENALTY or self.episode_step >= 200:
-            done = True
-
-        return new_observation, reward, done
-
-    def render(self, p=0.5):
-        img = self.get_image()
-        img = img.resize((300, 300))  # resizing so we can see our agent in all its glory.
-#        cv2.imshow("image", np.array(img))  # show it!
-#        cv2.waitKey(1)
-#        img = Image.fromarray(np.array(img), "RGB")
-#        img.show()
-        plt.imshow(np.array(img))
-        plt.show(block=False)
-        plt.pause(p)
-        plt.close()
-#        time.sleep(1)
-#        plt.close("all")
-#        input("press key to continue..")
-
-    # FOR CNN
-    def get_image(self):
-        env = np.zeros((self.SIZE, self.SIZE, 3), dtype=np.uint8)  # starts an rbg of our size
-        env[self.food.x][self.food.y] = self.d[self.FOOD_N]  # sets the food location tile to green color
-        env[self.enemy.x][self.enemy.y] = self.d[self.ENEMY_N]  # sets the enemy location to red
-        env[self.player.x][self.player.y] = self.d[self.PLAYER_N]  # sets the player tile to blue
-        img = Image.fromarray(env, 'RGB')  # reading to rgb. Apparently. Even tho color definitions are bgr. ???
-        return img
 
 
 class DQNAgent:
@@ -495,26 +337,6 @@ if __name__ == "__main__":
                     assert False
     
             if SHOW_PREVIEW and not episode % DEBUG_EVERY:
-#                go = ""
-#                if action == 0:
-#                    go = "BOTTOM_RIGHT"
-#                elif action == 1:
-#                    go = "TOP_LEFT"
-#                elif action == 2:
-#                    go = "TOP_RIGHT"
-#                elif action == 3:
-#                    go = "BOTTOM_LEFT"
-#                elif action == 4:
-#                    go = "RIGHT"
-#                elif action == 5:
-#                    go = "LEFT"
-#                elif action == 6:
-#                    go = "TOP"
-#                elif action == 7:
-#                    go = "BOTTOM"
-#                elif action == 8:
-#                    go = "STAY"
-#                print("moving {} by {} from {} to {}; got reward {}".format(go.ljust(12), a, current_state, new_state, reward))
                 print("taking {} {} (at {}) giving {} reward with total {} and done={} epsion={:.4f} || selected: {}{} || ".format(
                         a, action, phase, reward, episode_reward, done, epsilon, order_selection, order_complexities))
                 env.render()
